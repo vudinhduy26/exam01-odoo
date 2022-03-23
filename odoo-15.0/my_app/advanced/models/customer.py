95ea6f2ab7ff526f41dc4710eb9b775f22c38ad5
vudinhduy26 <pikaduy26@gmail.com>
2022-03-23 18:41:41 +0700

    up file

--- odoo-15.0/my_app/advanced/models/customer.py
+++ odoo-15.0/my_app/advanced/models/customer.py
@@ -1,5 +1,6 @@
-from odoo import fields , models ,api
+from odoo import fields, models, api
 import re
+
 
 class customer(models.Model):
     _description = 'sales'
@@ -7,8 +8,15 @@ class customer(models.Model):
 
     customer_discount_code = fields.Text(string='Customer discount')
     Sale_order_discount_estimated = fields.Integer(string="Discount Total")
+    discount = fields.Float(string="Discount % ", compute="_discount_customer")
+    #check = fields.Text(compute="check_discount_customer")
 
-    def discount_customer(self):
-        regex = re.compile(r'[0-9]+')
-        result = regex.search(self.customer_discount_code)
-        return result
+    @api.onchange('discount', 'customer_discount_code')
+    def _discount_customer(self):
+        pattern = re.compile(r"\d+")
+        if self.customer_discount_code:
+            for record in self:
+                k = pattern.search(record.customer_discount_code)
+                record.discount = float(k.group(0))
+
+
